8f4c7a4549d2ecaf3795dfec3dc60273aab64a65
Dzulfyy <dzulfikarfathin09@gmail.com>
2023-08-31 15:41:10 +0700

    menambahkan file tugas4.js di folder 04-javascript-loop-array

---
+++ 04-javascript-loop-array/tugas4.js
@@ -0,0 +1,59 @@
+// soal 1
+console.log("ini jawaban soal 1");
+for(i = 0; i < 10; i++){
+    console.log(i)
+};
+
+// soal 2
+console.log("ini jawaban soal 2");
+for(var i = 0; i<= 10; i++){
+    if(i % 2 !== 0){
+        console.log(i)
+    }
+};
+
+// soal 3
+console.log("ini jawaban soal 3");
+for(var i = 0; i < 10; i++){
+    if(i % 2 === 0){
+        console.log(i)
+    }
+};
+
+// soal 4
+console.log("ini jawaban soal 4");
+let array1 = [1,2,3,4,5,6];
+console.log(array1[5]);
+
+// soal 5
+console.log("ini jawaban soal 5");
+let array2 = [5,2,4,1,3,5];
+console.log(array2.sort());
+
+// soal 6
+console.log("ini jawaban nomoer 6");
+let array3 = ["selamat", "anda", "melakukan", "perulangan", "array", "dengan", "for"];
+
+for (let i = 0; i < array3.length; i++) {
+  console.log(array3[i]);
+}
+
+// soal 7
+console.log("ini jawaban nomer 7");
+let array4 = [1,2,3,4,5,6,7,8,9,10];
+for(i = 1; i < array4.length; i++){
+    if(i % 2 === 0){
+        console.log(i)
+    }
+};
+
+// soal 8
+console.log("ini jawaban nomer 8");
+let kalimat = ["saya", "sangat", "senang", "belajar", "javascript"];
+console.log(kalimat.join(" "));
+
+// soal 9
+console.log("ini jawaban nomer 9");
+var sayuran = [];
+sayuran.push('Kangkung', 'Bayam', 'Buncis', 'Kubis', 'Timun', 'Seledri', 'Tauge');
+console.log(sayuran);
