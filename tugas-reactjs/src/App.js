2d1a05ea93f98067eb909d6ffb8118c531d5ab5c
Dzulfyy <dzulfikarfathin09@gmail.com>
2023-09-05 15:12:48 +0700

    menambahkan tugas-reactjs

---
+++ tugas-reactjs/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="header">Hallo ,Saya Dzulfikar Fathin</h1>
+      <p>Saya dari Batch 49 sudah berhasil menginstall React</p>
+    </div>
+  );
+}
+
+export default App;
